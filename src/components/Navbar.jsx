60dcfe5df454130d3d6b36189ae03260bffbb5c4
ahmedabuamr <ahmed.n.abuamr@gmail.com>
2023-02-26 10:19:52 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav style={{width:"100%", padding:"10px 80px", background:"blue"}}>
         <div style={{ display:"flex", maxWidth:"800px",margin:"auto", color:"#fff", justifyContent:"space-between"}}>
-            <h2>Home</h2>
+           <Link to="/"><h2>Home</h2></Link>
            <Link to="/login"> <h2 style={{cursor:"pointer"}}>Login</h2></Link>
         </div>
     </nav>
